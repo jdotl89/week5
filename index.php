1563d475575d310bf291a0011a0f72f99c148554
John Laygo <jlaygo@mail.greenriver.edu>
2020-05-04 16:17:15 -0400

    initialized views composer index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// create an instance of the base class
+$f3 = Base::instance();
+
+// define a default route
+$f3->route('GET /', function(){
+    echo '<h1>week 5</h1>';
+
+/*    $view = new Template();
+    echo $view->render('views/home.html');*/
+});
+
+// run fat free
+$f3->run();
